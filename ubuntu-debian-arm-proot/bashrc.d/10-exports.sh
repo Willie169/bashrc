bb80ef94b4f5b50b807588a0c7cab843a75c621e
Willie Shen <cg10250207@gmail.com>
2026-01-17 22:48:27 +0800

    y

---
+++ ubuntu-debian-arm-proot/bashrc.d/10-exports.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export TMPDIR="/tmp"
+export PREFIX="/usr"
+export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/include"
+export NVM_DIR="$HOME/.nvm"
+export JAVA_HOME="/usr/lib/jvm/java-21-openjdk-arm64"
+export CLASSPATH="$CLASSPATH:/usr/local/java/antlr-4.13.2-complete.jar:/usr/local/java/plantuml.jar"
+export GOPROXY='direct'
+export GOROOT="/usr/local/go"
+export GOPATH="$HOME/go:$GOPATH"
+export PATH="$PATH:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$HOME/.local/bin:$HOME/.local/sbin:/usr/glibc/bin:$GOPATH/bin:$GOROOT/bin:$HOME/.cargo/bin:/usr/local/texlive/2025/bin/aarch64-linux"
+export PLANTUML_JAR="/usr/local/java/plantuml.jar"
+export KIT="/usr/share/LaTeX-ToolKit"
+export PATCH="$HOME/texmf/tex/latex/physics-patch"
+export PULSE_SERVER='127.0.0.1'
+export DLFLAGS='-A'
