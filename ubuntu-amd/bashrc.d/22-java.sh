3940cd8afad84c6686cbedc88cf96df7aec1dfab
Willie Shen <cg10250207@gmail.com>
2026-01-17 22:45:32 +0800

    y

--- ubuntu-amd/bashrc.d/22-java.sh
+++ ubuntu-amd/bashrc.d/22-java.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-alias antlr4='java -Xmx500M -cp "/usr/local/lib/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-alias grun='java -Xmx500M -cp "/usr/local/lib/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
+alias antlr4='java -Xmx500M -cp "/usr/local/java/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
+alias grun='java -Xmx500M -cp "/usr/local/java/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
