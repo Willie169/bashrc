3ecb4a7b7829de769d2de003db628634c4c19fed
Willie Shen <cg10250207@gmail.com>
2026-01-18 09:51:02 +0800

    y

--- termux-proot/bashrc.d/22-java.sh
+++
@@ -1,4 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-alias antlr4='java -Xmx500M -cp "$PREFIX/local/java/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-alias grun='java -Xmx500M -cp "$PREFIX/local/java/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
