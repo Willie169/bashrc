fecf1be024ce992adac8a72c4fbf209418b7a7a7
Willie Shen <cg10250207@gmail.com>
2026-01-18 15:44:29 +0800

    y

--- termux/bashrc.d/00-env.sh
+++ termux/bashrc.d/00-env.sh
@@ -6,9 +6,6 @@ HISTSIZE=5000
 HISTFILESIZE=10000
 shopt -s checkwinsize
 shopt -s globstar
-if command -v lesspipe >/dev/null 2>&1; then
-  eval "$(SHELL=/bin/sh lesspipe)"
-fi
 pulseaudio --start --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 pacmd load-module module-sles-sink
