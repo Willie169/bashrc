334179dd98dc5cc4298e465602bfea7418404e33
Willie Shen <cg10250207@gmail.com>
2026-01-19 09:03:30 +0800

    y

--- ubuntu-amd/bashrc.d/bashrc.sh
+++ ubuntu-amd/bashrc.d/bashrc.sh
@@ -10,3 +10,11 @@ if [ -d "$HOME/.bashrc.d"  ];  then
     [ -r "$f"  ] && . "$f"
   done
 fi
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export MAMBA_ROOT_PREFIX="${HOME}/conda"
+source "${HOME}/conda/etc/profile.d/conda.sh"
+source "${HOME}/conda/etc/profile.d/mamba.sh"
