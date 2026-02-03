d696f248f39a3cdc2fe7e7a690b41562881e5311
Willie Shen <cg10250207@gmail.com>
2026-02-03 17:15:48 +0800

    l

--- termux/bashrc.d/bashrc.sh
+++ termux/bashrc.d/bashrc.sh
@@ -11,6 +11,8 @@ if [ -d "$HOME/.bashrc.d"  ];  then
   done
 fi
 
+[ -r ~/API_KEY.sh ] && source ~/API_KEY.sh
+
 pulseaudio --start --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 pacmd load-module module-sles-sink
