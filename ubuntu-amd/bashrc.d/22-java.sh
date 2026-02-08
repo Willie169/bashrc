26ee990d4745fb284b6f72246f2710591c12e3ad
Willie Shen <cg10250207@gmail.com>
2026-02-08 13:48:10 +0800

    a

--- ubuntu-amd/bashrc.d/22-java.sh
+++ ubuntu-amd/bashrc.d/22-java.sh
@@ -2,4 +2,4 @@
 
 alias antlr4='java -Xmx500M -cp "/usr/local/java/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
 alias grun='java -Xmx500M -cp "/usr/local/java/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
-alias lizzieyzy='java -jar $HOME/lizzieyzy/target/lizzie-yzy2.5.3-shaded.jar'
+alias lizzieyzy='java -jar "$HOME/lizzieyzy/target/lizzie-yzy2.5.3-shaded.jar"'
