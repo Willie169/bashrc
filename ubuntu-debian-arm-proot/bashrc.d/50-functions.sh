7caec08ea44ddcde3b64f41d562a4e0bfa8e9b6c
Willie Shen <cg10250207@gmail.com>
2026-02-09 18:31:48 +0800

    l

--- ubuntu-debian-arm-proot/bashrc.d/50-functions.sh
+++ ubuntu-debian-arm-proot/bashrc.d/50-functions.sh
@@ -1110,7 +1110,7 @@ update_lizzieyzy_config() {
   rm $HOME/.lizzieyzy/config.txt
   wget https://raw.githubusercontent.com/Willie169/bashrc/main/lizzieyzy/config.txt -O $HOME/.lizzieyzy/config.txt
   envsubst < $HOME/.lizzieyzy/config.txt > $HOME/.lizzieyzy/config.txt
-  sed -e "s|\$(nproc)|$(nproc)|g" $HOME/.lizzieyzy/config.txt > $HOME/.lizzieyzy/config.txt
+  sed -e "s|\$(HOME)|$(HOME)|g" -e "s|\$(nproc)|$(nproc)|g" $HOME/.lizzieyzy/config.txt
   )
 }
 
