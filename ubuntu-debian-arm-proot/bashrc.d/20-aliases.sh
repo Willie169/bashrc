6658a08cc40fb2766d3576807e93ef1611de61df
Willie Shen <cg10250207@gmail.com>
2026-02-20 23:07:49 +0800

    y

--- ubuntu-debian-arm-proot/bashrc.d/20-aliases.sh
+++ ubuntu-debian-arm-proot/bashrc.d/20-aliases.sh
@@ -10,5 +10,5 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 alias src='source'
 alias httpp='http-server -p'
 alias sshd='/usr/sbin/sshd'
-alias open-notebook='(cd ~/.open-notebook && udocker compose up -d)'
-alias open-notebook-down='(cd ~/.open-notebook && udocker compose down)'
+alias open-notebook='(cd ~/.open-notebook && udocker --allow-root compose up -d)'
+alias open-notebook-down='(cd ~/.open-notebook && udocker --allow-root compose down)'
