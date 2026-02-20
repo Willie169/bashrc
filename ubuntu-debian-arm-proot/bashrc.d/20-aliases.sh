2fec799616ff4287913b09abc498c613d9822dc7
Willie Shen <cg10250207@gmail.com>
2026-02-21 01:03:35 +0800

    a

--- ubuntu-debian-arm-proot/bashrc.d/20-aliases.sh
+++ ubuntu-debian-arm-proot/bashrc.d/20-aliases.sh
@@ -10,6 +10,3 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 alias src='source'
 alias httpp='http-server -p'
 alias sshd='/usr/sbin/sshd'
-alias udocker='udocker --allow-root'
-alias open-notebook='(cd ~/.open-notebook && udocker run surrealdb start --log info --user root --pass root rocksdb:~/.open-notebook/surreal_data/mydatabase.db 2>/dev/null & && SURREAL_PID=$! && echo $SURREAL_PID > surrealdb.pid && sleep 5 && OPEN_NOTEBOOK_ENCRYPTION_KEY=change-me-to-a-secret-string SURREAL_URL=ws://localhost:8000/rpc SURREAL_USER=root SURREAL_PASSWORD=root SURREAL_NAMESPACE=open_notebook SURREAL_DATABASE=open_notebook OLLAMA_API_BASE=http://localhost:11434 udocker run open-notebook 2>/dev/null & && NOTEBOOK_PID=$! && echo $NOTEBOOK_PID > open-notebook.pid)'
-alias open-notebook-stop='(cd ~/.open-notebook && [ -f surrealdb.pid ] && kill $(cat surrealdb.pid) 2>/dev/null && [ -f open-notebook.pid ] && kill $(cat open-notebook.pid) 2>/dev/null && rm -f surrealdb.pid open-notebook.pid)'
