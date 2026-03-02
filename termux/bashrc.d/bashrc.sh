901dbae8c4c5686c1c067803cc33f26329013c1b
Willie169 <cg10250207@gmail.com>
2026-03-02 17:05:54 +0800

    y

--- termux/bashrc.d/bashrc.sh
+++ termux/bashrc.d/bashrc.sh
@@ -16,4 +16,5 @@ fi
 pulseaudio --start --exit-idle-time=-1
 pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 pacmd load-module module-sles-sink
-virgl_test_server_android &
+
+MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &
