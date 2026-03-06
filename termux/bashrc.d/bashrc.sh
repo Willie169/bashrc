7858f1901c3e43102c6948c8ef88ade694268a1c
Willie169 <cg10250207@gmail.com>
2026-03-06 11:05:00 +0800

    y

--- termux/bashrc.d/bashrc.sh
+++ termux/bashrc.d/bashrc.sh
@@ -19,7 +19,7 @@ pacmd load-module module-sles-sink
 
 MESA_NO_ERROR=1 MESA_LOADER_DRIVER_OVERRIDE=zink MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &
 
-SOCKET=/data/data/com.termux/files/usr/tmp/termux-shell.sock
+export SOCKET=/data/data/com.termux/files/usr/tmp/termux-shell.sock
 if command -v socat >/dev/null 2>&1; then
 if [ ! -S "$SOCKET" ] || ! echo "echo hello" | socat - UNIX-CONNECT:"$SOCKET" 2>/dev/null | grep -q "^hello$"; then
 mkdir -p "$(dirname "$SOCKET")"
