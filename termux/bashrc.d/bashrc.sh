cd99eb92a487ad7e57ab584650d5650259f46a0a
Willie169 <cg10250207@gmail.com>
2026-03-06 10:42:36 +0800

    y

--- termux/bashrc.d/bashrc.sh
+++ termux/bashrc.d/bashrc.sh
@@ -24,6 +24,15 @@ if command -v socat >/dev/null 2>&1; then
   if [ ! -S "$SOCKET" ] || ! echo "echo hello" | socat - UNIX-CONNECT:"$SOCKET" 2>/dev/null | grep -q "^hello$"; then
     mkdir -p "$(dirname "$SOCKET")"
     [ -e "$SOCKET" ] && rm "$SOCKET"
-    socat UNIX-LISTEN:"$SOCKET",fork,reuseaddr SYSTEM:'read line; if [ -n "$line" ]; then eval "$line"; fi' >/dev/null 2>&1 &
+    socat UNIX-LISTEN:"$SOCKET",fork,reuseaddr SYSTEM:'
+      export PREFIX=/data/data/com.termux/files/usr
+      export PATH=$PATH
+      export HOME=$HOME
+      while read line; do
+        if [ -n "$line" ]; then
+          eval "$line"
+        fi
+      done
+    ' >/dev/null 2>&1 &
   fi
 fi
