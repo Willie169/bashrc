eba8258fbf2bc2f362d62efae8f84b0b68f3ff50
Willie169 <cg10250207@gmail.com>
2026-03-11 15:41:20 +0800

    nvim

--- nvim.sh
+++ nvim.sh
@@ -70,8 +70,8 @@ return {
       },
     }
 
-    vim.keymap.set("n", "<leader>CC", "<cmd>CodeCompanionChat<CR>", { desc = "Open Code Companion Chat" })
-    vim.keymap.set("n", "<leader>CI", "<cmd>CodeCompanion<CR>", { desc = "Open Code Companion Inline" })
+    vim.keymap.set("n", "<leader>vc", "<cmd>CodeCompanionChat<CR>", { desc = "Open Code Companion Chat" })
+    vim.keymap.set("n", "<leader>vi", "<cmd>CodeCompanion<CR>", { desc = "Open Code Companion Inline" })
 
   end,
 }
