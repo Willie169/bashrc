9fc66a7e4d2b153ba96f15847ed8f9fa31c3ce6b
Willie169 <cg10250207@gmail.com>
2026-03-11 16:11:09 +0800

    nvim

--- nvim.sh
+++ nvim.sh
@@ -34,6 +34,7 @@ return {
   "olimorris/codecompanion.nvim",
   version = "^19.0.0",
   lazy = false,
+  opts = {},
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
@@ -87,6 +88,18 @@ return {
         },
         cmd = {
           adapter = "ollama_remote",
+        },
+      },
+      opts = {
+        log_level = 'DEBUG',
+      },
+      display = {
+        diff = {
+          enabled = true,
+          close_chat_at = 240, -- Close an open chat buffer if the total columns of your display are less than...
+          layout = 'vertical', -- vertical|horizontal split for default provider
+          opts = { 'internal', 'filler', 'closeoff', 'algorithm:patience', 'followwrap', 'linematch:120' },
+          provider = 'default', -- default|mini_diff
         },
       },
     }
