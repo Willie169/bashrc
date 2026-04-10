3a914927ca5b8fbf373a31275d378a5eaa75a85f
Willie169 <cg10250207@gmail.com>
2026-04-10 16:21:41 +0800

    u

--- ubuntu-debian-arm-proot/bashrc.d/bashrc.sh
+++ ubuntu-debian-arm-proot/bashrc.d/bashrc.sh
@@ -17,6 +17,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
+
 export MAMBA_ROOT_PREFIX="${HOME}/conda"
 source "${HOME}/conda/etc/profile.d/conda.sh" 2>/dev/null || true
 source "${HOME}/conda/etc/profile.d/mamba.sh" 2>/dev/null || true
