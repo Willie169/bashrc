b32240d25c9447f1ebc1b72f8d5059ae303dfc31
Willie169 <cg10250207@gmail.com>
2026-05-25 09:08:51 +0800

    y

--- ubuntu-debian-arm-proot/bashrc.d/10-exports.sh
+++ ubuntu-debian-arm-proot/bashrc.d/10-exports.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="${HOME}/.nvm"
 export MAMBA_ROOT_PREFIX="${HOME}/conda"
 export TMPDIR="/tmp"
 export JAVA_HOME="/usr/lib/jvm/java-21-openjdk-arm64"
 export JAVAROOT="/usr/local/java"
-export CLASSPATH="$CLASSPATH:/usr/local/java/antlr-4.13.2-complete.jar:/usr/local/java/plantuml.jar"
+export CLASSPATH="${CLASSPATH}:/usr/local/java/antlr-4.13.2-complete.jar:/usr/local/java/plantuml.jar"
 export GOPROXY='direct'
 export GOROOT="/usr/local/go"
-export GOPATH="$HOME/go"
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$GOPATH/bin:$HOME/.cargo/bin:/usr/local/texlive/2026/bin/aarch64-linux:/usr/local/texlive/2025/bin/aarch64-linux:$HOME/dev/llm/llama.cpp/build/bin:$HOME/KataGo/cpp:$HOME/Fairy-Stockfish/src:$HOME/cutechess/build:$HOME/Sylvan/projects/gui:$HOME/Sylvan/projects/cli:$BUN_INSTALL/bin:$JAVA_HOME/bin:$HOME/eclipse.jdt.ls/bin:$HOME/.local/bin:$HOME/.opencode/bin:$HOME/.local/sbin:$GOROOT/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
+export GOPATH="${HOME}/go"
+export BUN_INSTALL="${HOME}/.bun"
+export PATH="${HOME}/deb-get:$GOPATH/bin:${HOME}/.cargo/bin:/usr/local/texlive/2026/bin/aarch64-linux:/usr/local/texlive/2025/bin/aarch64-linux:${HOME}/dev/llm/llama.cpp/build/bin:${HOME}/KataGo/cpp:${HOME}/Fairy-Stockfish/src:${HOME}/cutechess/build:${HOME}/Sylvan/projects/gui:${HOME}/Sylvan/projects/cli:${BUN_INSTALL}/bin:${JAVA_HOME}/bin:${HOME}/eclipse.jdt.ls/bin:${HOME}/.local/bin:${HOME}/.opencode/bin:${HOME}/.local/sbin:$GOROOT/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:${PATH}"
 export ANTLR_JAR="/usr/local/java/antlr-4.13.2-complete.jar"
 export PLANTUML_JAR="/usr/local/java/plantuml.jar"
 export KIT="/usr/share/LaTeX-ToolKit"
-export PATCH="$HOME/texmf/tex/latex/physics-patch"
+export PATCH="${HOME}/texmf/tex/latex/physics-patch"
 export VISUAL=nvim
-export EDITOR="$VISUAL"
-export AIDER_EDITOR="$VISUAL"
+export EDITOR="${VISUAL}"
+export AIDER_EDITOR="${VISUAL}"
 export OLLAMA_API_BASE=http://127.0.0.1:11434
 export OLLAMA_API_HOST=http://127.0.0.1:11434
 export OLLAMA_BASE_URL=http://127.0.0.1:11434
