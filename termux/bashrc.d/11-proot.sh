24586bea945926aac44b72b91c08a28cfa2d3047
Willie169 <cg10250207@gmail.com>
2026-06-16 17:10:57 +0800

    y

--- termux/bashrc.d/11-proot.sh
+++ termux/bashrc.d/11-proot.sh
@@ -1,12 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-export TERMUX="termux"
-export UBUNTU="ubuntu"
-export DEBIAN="debian"
-export UBUNTUBOX="ubuntubox"
-export DEBIANBOX="debianbox"
+export TERMUX=termux
 export TERMUX_HOME="/data/data/com.termux/files/usr/var/lib/proot-distro/containers/$TERMUX/rootfs/data/data/com.termux/files/home"
+export UBUNTU=ubuntu
 export UBUNTU_ROOT="/data/data/com.termux/files/usr/var/lib/proot-distro/containers/$UBUNTU/rootfs/root"
+export DEBIAN=debian
 export DEBIAN_ROOT="/data/data/com.termux/files/usr/var/lib/proot-distro/containers/$DEBIAN/rootfs/root"
+export UBUNTUBOX=ubuntubox
 export UBUNTUBOX_ROOT="/data/data/com.termux/files/usr/var/lib/proot-distro/containers/$UBUNTUBOX/rootfs/root"
+export DEBIANBOX=debianbox
 export DEBIANBOX_ROOT="/data/data/com.termux/files/usr/var/lib/proot-distro/containers/$DEBIANBOX/rootfs/root"
