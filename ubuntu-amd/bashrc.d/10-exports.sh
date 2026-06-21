4272f3169b272457efeca0238eab14210cacd11b
Willie169 <cg10250207@gmail.com>
2026-06-21 18:35:26 +0800

    y

--- ubuntu-amd/bashrc.d/10-exports.sh
+++ ubuntu-amd/bashrc.d/10-exports.sh
@@ -23,6 +23,7 @@ export WAYDROID="${HOME}/.local/share/waydroid/data/media/0"
 export ANTLR_JAR="/usr/local/java/antlr-4.13.2-complete.jar"
 export PLANTUML_JAR="/usr/local/java/plantuml.jar"
 export BOTTLES="${HOME}/.var/app/com.usebottles.bottles/data/bottles"
+export PRF="${HOME}/.local/share/proot-distro/containers"
 export MINETEST="${HOME}/.var/app/org.luanti.luanti/.minetest"
 export CC_PORT=8082
 export KIT="/usr/share/LaTeX-ToolKit"
