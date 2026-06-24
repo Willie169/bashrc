59109f2277dc7f7a6dcacc7651771ee59c720fd4
Willie Shen <cg10250207@gmail.com>
2026-06-25 02:45:57 +0800

    y

--- ubuntu-amd/bashrc.d/15-color.sh
+++ ubuntu-amd/bashrc.d/15-color.sh
@@ -8,7 +8,7 @@ color_prompt=yes
 
 #force_color_prompt=yes
 
-if [ -n "$force_color_prompt" ]; then
+if [ -n "${force_color_prompt:-}" ]; then
   if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
     color_prompt=yes
   else
@@ -16,14 +16,14 @@ if [ -n "$force_color_prompt" ]; then
   fi
 fi
 
-if [ "$color_prompt" = yes ]; then
+if [ "${color_prompt:-}" = yes ]; then
   PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 else
   PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
 fi
 unset color_prompt force_color_prompt
 
-case "$TERM" in
+case "${TERM:-}" in
   xterm*|rxvt*)
     PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
     ;;
