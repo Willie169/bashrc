ac3062ad5bb6975eabc72363597b1cb7ce0eca82
Willie Shen <cg10250207@gmail.com>
2026-06-25 17:50:17 +0800

    y

--- ubuntu-debian-arm-proot/bashrc.d/20-aliases.sh
+++ ubuntu-debian-arm-proot/bashrc.d/20-aliases.sh
@@ -11,6 +11,7 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 alias src='source'
 alias sbrc='source ~/.bashrc'
 alias py='python3'
+alias dfur='df --output=used / | tail -n1'
 alias httpp='http-server -p'
 alias lbtr='libretranslate &'
 alias yt-sub='yt-dlp --write-subs --sub-format vtt  --skip-download'
