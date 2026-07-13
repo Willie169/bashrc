996454f4fbd2c46f18e5652aa43e5eca243c8ce5
Willie Shen <cg10250207@gmail.com>
2026-07-13 20:46:02 +0800

    l

--- ubuntu-amd/bashrc.d/50-functions.sh
+++ ubuntu-amd/bashrc.d/50-functions.sh
@@ -1660,11 +1660,6 @@ update_tools() {
   tar -xJf ClipCascade_Linux.tar.xz
   rm ClipCascade_Linux.tar.xz
   [ -f ~/.ClipCascade.DATA ] && mv ~/.ClipCascade.DATA ~/ClipCascade/DATA
-  rm -rf ~/ventoy
-  gh_latest -w --wget_option '--tries=100 --retry-connrefused --waitretry=5' ventoy/ventoy 'ventoy-*-linux.tar.gz'
-  tar -xzf ventoy-*-linux.tar.gz
-  rm ventoy-*-linux.tar.gz
-  mv ventoy-* ventoy
   rm -f ~/.local/bin/rclone
   gh_latest -w --wget_option '--tries=100 --retry-connrefused --waitretry=5' gulp79/rclone-extra rclone-linux-amd64.zip
   unzip rclone-linux-amd64.zip
