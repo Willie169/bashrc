20f20184c84296d127252737c3ffbb21004bd289
Willie Shen <cg10250207@gmail.com>
2026-07-30 19:55:13 +0800

    l

--- ci.sh
+++ ci.sh
@@ -2,12 +2,32 @@
 
 shopt -s globstar
 dir="$(cd -- "$(dirname -- "$0")" && pwd)"
-arr=( "termux" "ubuntu-amd" "ubuntu-debian-arm-proot" )
+arr=("termux" "ubuntu-amd" "ubuntu-debian-arm-proot")
 for d in "${arr[@]}"; do
+	u="$dir/$d"/update.sh
+	echo '#!/usr/bin/env bash
 
+cd ~ || exit
+rm -f .bashrc 2>/dev/null || true
+rm -rf .bashrc.d 2>/dev/null || true
+mkdir .bashrc.d
+cd .bashrc.d || exit' >"$u"
+	for f in "$dir"/bashrc.d/*.sh; do
+		echo 'wget https://raw.githubusercontent.com/Willie169/bashrc/main/bashrc.d/'"$(basename "$f")" >>"$u"
+	done
+	for f in "$dir/$d"/bashrc.d/*.sh; do
+		echo 'wget https://raw.githubusercontent.com/Willie169/bashrc/main/'"$d"'/bashrc.d/'"$(basename "$f")" >>"$u"
+	done
+	echo 'cd ~ || exit
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/'"$d"'/bashrc.d/bashrc -O .bashrc' >>"$u"
+done
 for f in "$dir"/**/*.sh "$dir"/**/bashrc; do
 	chmod +x "$f"
-	shfmt -w "$f"
-	perl -0777 -i -pe 's/\n*\z/\n/' "$f"
-	shellcheck "$f"
+	#shfmt -w "$f"
+	#shellcheck "$f"
+done
+
+for f in "$dir"/**/*.py; do
+	chmod +x "$f"
+	autopep8 --in-place --aggressive --aggressive "$f"
 done
---
+++ termux/update.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cd ~ || exit
+rm -f .bashrc 2>/dev/null || true
+rm -rf .bashrc.d 2>/dev/null || true
+mkdir .bashrc.d
+cd .bashrc.d || exit
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/bashrc.d/20-shared-aliases.sh
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/bashrc.d/50-shared-functions.sh
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/termux/bashrc.d/00-env.sh
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/termux/bashrc.d/10-exports.sh
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/termux/bashrc.d/11-proot.sh
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/termux/bashrc.d/21-aliases.sh
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/termux/bashrc.d/22-vnc.sh
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/termux/bashrc.d/51-functions.sh
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/termux/bashrc.d/52-extra-functions.sh
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/termux/bashrc.d/60-completion.sh
+cd ~ || exit
+wget https://raw.githubusercontent.com/Willie169/bashrc/main/termux/bashrc.d/bashrc -O .bashrc
