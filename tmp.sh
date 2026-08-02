bd57534120f8a81ac5a902577b433e49b9e2684d
Willie Shen <cg10250207@gmail.com>
2026-08-03 01:14:42 +0800

    2026-08-02T17:14:42+00:00

--- tmp.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-shopt -s globstar
-
-dir="$(cd -- "$(dirname -- "$0")" && pwd)"
-
-for f in "$dir"/**/*; do
-	test -f "$f" && sed -i 's/gh_release -r/gh_release -r/g; s/gh_release/gh_release/g' "$f"
-done
