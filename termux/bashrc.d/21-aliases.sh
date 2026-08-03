c3e6551499cc4aeecbdd9b82e273c9ce58105379
Willie Shen <cg10250207@gmail.com>
2026-08-04 00:26:21 +0800

    2026-08-03T16:26:21+00:00

--- termux/bashrc.d/21-aliases.sh
+++ termux/bashrc.d/21-aliases.sh
@@ -1,8 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # shellcheck disable=2142
+alias getprop='/system/bin/getprop'
 alias dfur='df $(dirname "$PREFIX") | tail -n1 | awk '"'"'{print $3}'"'"
 alias ccp='termux-clipboard-set'
 alias cpt='termux-clipboard-get'
-alias antlr4='java -Xmx500M -cp "$PREFIX/local/java/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.Tool'
-alias grun='java -Xmx500M -cp "$PREFIX/local/java/antlr-4.13.2-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig'
