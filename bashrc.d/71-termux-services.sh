fa744e85be110595523ff52d93e5da7feb644b08
Willie Shen <cg10250207@gmail.com>
2026-08-08 01:04:17 +0800

    2026-08-07T17:04:17+00:00

--- bashrc.d/71-termux-services.sh
+++ bashrc.d/71-termux-services.sh
@@ -12,12 +12,12 @@ if [ ! -f ~/.bashrc.pid/pulseaudio.pid ] || ! kill -0 "$(cat ~/.bashrc.pid/pulse
 		pgrep -x pulseaudio | head -n1 >~/.bashrc.pid/pulseaudio.pid
 		pacmd load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1
 		pacmd load-module module-sles-sink
-	)
+	) || true
 fi
 
 if [ ! -f ~/.bashrc.pid/virgl_test_server.pid ] || ! kill -0 "$(cat ~/.bashrc.pid/virgl_test_server.pid)" 2>/dev/null; then
 	command -v virgl_test_server >/dev/null 2>&1 && (
 		MESA_NO_ERROR=1 MESA_LOADER_DRIVER_OVERRIDE=zink MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles >/dev/null 2>&1 &
 		echo $! >~/.bashrc.pid/virgl_test_server.pid
-	)
+	) || true
 fi
