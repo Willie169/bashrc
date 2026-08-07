b37728ad092431043a4ea26a4bf6eda37323b8a4
Willie Shen <cg10250207@gmail.com>
2026-08-07 15:53:13 +0800

    2026-08-07T07:53:13+00:00

--- ci.sh
+++ ci.sh
@@ -18,7 +18,7 @@ done
 arr=("termux" "ubuntu-amd" "ubuntu-debian-arm-proot")
 for d in "${arr[@]}"; do
 	u="$dir/bashrc.d/$d/install.sh"
-	echo $'#!/usr/bin/env sh\n\ncd ~ || exit\nrm -f .bashrc 2>/dev/null || true\nrm -rf .bashrc.d 2>/dev/null || true\nmkdir .bashrc.d\ncd .bashrc.d || exit' >"$u"
+	echo $'#!/usr/bin/env sh\n\ncd ~ || exit\nrm -f .bashrc 2>/dev/null || true\nrm -rf .bashrc.d 2>/dev/null || true\ngit clone --depth=1 https://github.com/Willie169/bashrc.git ~/.bashrc.d || exit' >"$u"
 	for f in "$dir"/bashrc.d/shared/bashrc.d/*.sh; do
 		echo "wget https://raw.githubusercontent.com/Willie169/bashrc/main/bashrc.d/shared/bashrc.d/$(basename "$f")" >>"$u"
 	done
