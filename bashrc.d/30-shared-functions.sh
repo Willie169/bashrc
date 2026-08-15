4bec501c7c213c89f61f12b7028e59b90e6aab42
Willie Shen <cg10250207@gmail.com>
2026-08-15 17:46:12 +0800

    2026-08-15T09:46:12+00:00

--- bashrc.d/30-shared-functions.sh
+++ bashrc.d/30-shared-functions.sh
@@ -2077,11 +2077,11 @@ ffmpeg_segment() {
 }
 
 ffmpeg_concat_auto() {
-	printf "file '%s'\n" "$@" | ffmpeg -f concat -safe 0 -i - pipe:0 -c copy "$(echo "$1" | remove_extension | sed -E 's/_ffmpeg_[0-9]+$//').$(echo "$1" | get_extension)"
+	ffmpeg -f concat -safe 0 -i <(printf "file '%s'\n" "$@") pipe:0 -c copy "$(echo "$1" | remove_extension | sed -E 's/_ffmpeg_[0-9]+$//').$(echo "$1" | get_extension)"
 }
 
 ffmpeg_concat() {
-	printf "file '%s'\n" "${@:2}" | ffmpeg -f concat -safe 0 -i - pipe:0 -c copy "$1"
+	ffmpeg -f concat -safe 0 -i <(printf "file '%s'\n" "${@:2}") pipe:0 -c copy "$1"
 }
 
 jxl_lossy() {
