e1a355c83f70f8194a9d17314f2b464b84f6b1bc
Willie Shen <cg10250207@gmail.com>
2026-08-16 19:05:46 +0800

    2026-08-16T11:05:46+00:00

--- bashrc.d/60-external.sh
+++ bashrc.d/60-external.sh
@@ -1,33 +1,13 @@
 #!/usr/bin/env bash
 
-if ! shopt -oq posix; then
-	[ -f "$USR_DIR"/share/bash-completion/bash_completion ] && . "$USR_DIR"/share/bash-completion/bash_completion
-	[ -f "$ETC_DIR"/bash_completion ] && . "$ETC_DIR"/bash_completion
-fi
-
 [ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
 
-if [ -r "${HOME}/.nvm" ]; then
-	export NVM_DIR="${HOME}/.nvm"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-fi
+[ -n "${NVM_DIR:-}" ] && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+
+[ -n "${MAMBA_ROOT_PREFIX:-}" ] && source "$MAMBA_ROOT_PREFIX/etc/profile.d/conda.sh"
+[ -n "${MAMBA_ROOT_PREFIX:-}" ] && source "$MAMBA_ROOT_PREFIX/etc/profile.d/mamba.sh"
 
 [ -f "${HOME}"/.config/broot/launcher/bash/br ] && source "${HOME}"/.config/broot/launcher/bash/br
-
-command -v luarocks >/dev/null 2>&1 && eval <(luarocks completion bash)
-
-[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)"
-
-command -v fzf >/dev/null 2>&1 && eval "$(fzf --bash)"
-
-command -v llama-cli >/dev/null 2>&1 && eval "$(llama-cli --completion-bash)"
-
-if [ -r "${HOME}/conda" ]; then
-	export MAMBA_ROOT_PREFIX="${HOME}/conda"
-	source "${HOME}/conda/etc/profile.d/conda.sh" 2>/dev/null || true
-	source "${HOME}/conda/etc/profile.d/mamba.sh" 2>/dev/null || true
-fi
 
 # shellcheck disable=2155,2164
 if command -v yazi >/dev/null 2>&1; then
@@ -39,3 +19,23 @@ if command -v yazi >/dev/null 2>&1; then
 		command rm -f -- "$tmp"
 	}
 fi
+
+case $- in
+*i*) ;;
+*) return ;;
+esac
+
+if ! shopt -oq posix; then
+	[ -f "$USR_DIR"/share/bash-completion/bash_completion ] && . "$USR_DIR"/share/bash-completion/bash_completion
+	[ -f "$ETC_DIR"/bash_completion ] && . "$ETC_DIR"/bash_completion
+fi
+
+[ -n "${NVM_DIR:-}" ] && [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
+command -v luarocks >/dev/null 2>&1 && source <(luarocks completion bash)
+
+[ -f /home/linuxbrew/.linuxbrew/bin/brew ] && source <(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)
+
+command -v fzf >/dev/null 2>&1 && source <(fzf --bash)
+
+command -v llama-cli >/dev/null 2>&1 && source <(llama-cli --completion-bash)
