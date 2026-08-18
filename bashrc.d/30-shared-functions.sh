8fcffe6e7b473a5d87d6f4649aac8ae7a5d15c86
Willie Shen <cg10250207@gmail.com>
2026-08-18 21:28:19 +0800

    2026-08-18T13:28:19+00:00

--- bashrc.d/30-shared-functions.sh
+++ bashrc.d/30-shared-functions.sh
@@ -2264,5 +2264,5 @@ cesh() {
 }
 
 fortune_cow() {
-  fortune | fmt -40 -s | $(shuf -n 1 -e cowsay cowthink) -$(shuf -n 1 -e b d g p s t w y) -f $(shuf -n 1 -e $(cowsay -l | tail -n +2)) -n
+  fortune | fmt -40 -s | "$(shuf -n 1 -e cowsay cowthink)" -"$(shuf -n 1 -e b d g p s t w y)" -f "$(shuf -n 1 -e "$(cowsay -l | tail -n +2)")" -n
 }
