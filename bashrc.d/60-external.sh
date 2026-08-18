b96d1be6509250b0f8e1cd0050825073f15e08b4
Willie Shen <cg10250207@gmail.com>
2026-08-18 18:01:00 +0800

    2026-08-18T10:01:00+00:00

--- bashrc.d/60-external.sh
+++ bashrc.d/60-external.sh
@@ -37,7 +37,3 @@ command -v luarocks >/dev/null 2>&1 && source <(luarocks completion bash)
 [ -f /home/linuxbrew/.linuxbrew/bin/brew ] && source <(/home/linuxbrew/.linuxbrew/bin/brew shellenv bash)
 
 command -v fzf >/dev/null 2>&1 && source <(fzf --bash)
-
-command -v llama-cli >/dev/null 2>&1 && source <(llama-cli --completion-bash 2>/dev/null)
-
-command -v hf >/dev/null 2>&1 && source <(hf --show-completion)
