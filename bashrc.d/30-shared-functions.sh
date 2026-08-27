829909dfd588cb1dc49ae374cc10edf687f2238a
Willie Shen <cg10250207@gmail.com>
2026-08-28 00:28:23 +0800

    2026-08-27T16:28:23+00:00

--- bashrc.d/30-shared-functions.sh
+++ bashrc.d/30-shared-functions.sh
@@ -2372,7 +2372,7 @@ multimedia_convert() {
               rm -- "$file"
           )
           ;;
-        *.avif | *.bmp | *.heic | *.ico | *.jp2 | *.tif | *.webp)
+        *.avif | *.bmp | *.heic | *.heif | *.ico | *.jbg | *.jbig | *.jp2 | *.tif | *.webp)
           local png="${file%.*}.png"
           if [[ -e "$dir/$png" ]]; then
             printf 'Skipping: intermediate png exists: %s\n' "$f" >&2
