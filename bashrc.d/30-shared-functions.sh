a89d319ad5ad181d4cf062b2c6ecf0abd6cd6141
Willie Shen <cg10250207@gmail.com>
2026-08-28 03:41:30 +0800

    2026-08-27T19:41:30+00:00

--- bashrc.d/30-shared-functions.sh
+++ bashrc.d/30-shared-functions.sh
@@ -2342,7 +2342,7 @@ multimedia_convert() {
       local dir=${f%/*}
       [[ $dir == "$f" ]] && dir=.
       case ${file,,} in
-        *_ffmpeg_av1_*_*_opus_*.mkv | *_ffmpeg_av1_lossless_flac*.mkv | *_ffmpeg_opus_*.opus | *_ffmpeg_flac*.flac)
+        *_ffmpeg_av1_*_*_opus_*.mkv | *_ffmpeg_av1_lossless_flac*.mkv)
           continue
           ;;
         *.ico)
